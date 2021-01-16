0cb7a5b7b00b1f0c1f667136233c1698001ce423
stefian <v_ianta@yahoo.com>
2021-01-16 10:39:34 -0400

    + PRO

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,7 @@
 <template>
   <header id="header">
     <div class="left">
-      <router-link to="/"><img id="logo" src="../assets/icon_154-white.png">CO</router-link>
+      <router-link to="/"><img id="logo" src="../assets/icon_154-white.png">PRO</router-link>
     </div>
     <div class="center">
       <router-link to="/">Monetizer</router-link>
