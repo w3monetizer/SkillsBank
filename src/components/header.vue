e59516b94b91ddc5258d993d53676970a3e4cde1
stefian <v_ianta@yahoo.com>
2021-02-21 20:30:16 -0400

    + <title>Fair Monetizer PoC</title>

--- src/components/header.vue
+++ src/components/header.vue
@@ -4,7 +4,7 @@
       <router-link to="/"><img id="logo" src="../assets/icon_154-white.png">fair</router-link>
     </div>
     <div class="center">
-      <router-link to="/">MONEY MAKER</router-link>
+      <router-link to="/">Monetizer</router-link>
     </div>
     <nav class="right">
       <ul>
